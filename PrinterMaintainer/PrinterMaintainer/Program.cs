5a89de3f5d81ac8e400ba9a65bc45a7f6200555a
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-11 09:34:05 +0100

    Added message boxes.

--- PrinterMaintainer/PrinterMaintainer/Program.cs
+++ PrinterMaintainer/PrinterMaintainer/Program.cs
@@ -6,11 +6,25 @@ using System.Windows.Forms;
 
 namespace PrinterMaintainer
 {
-    static class Program
+    internal static class Program
     {
+        private const string ConfigFilename = "config.yml";
+        private const string LastPrintTimeFilename = "lastPrintTime.dat";
+
         static void Main()
         {
-            
+            try
+            {
+                PrinterMaintainer printerMaintainer = new PrinterMaintainer(ConfigFilename, LastPrintTimeFilename);
+                if (printerMaintainer.ShouldPrintTestPage())
+                {
+                    DialogResult result = MaintainerMessages.ShowQuestion();
+                }
+            }
+            catch (Exception e)
+            {
+                MaintainerMessages.ShowError(e.ToString());
+            }
         }
     }
 }
